e0096a0734700835413e650cb2557c3efb97c139
Mnording <mattiasnording@gmail.com>
2015-03-22 19:25:19 +0100

    adding tests

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
-require
+/*jslint node: true */
+'use strict';
+var MongoClient = require('mongodb').MongoClient;
+module.exports = {
+    MongoReporter : function (options) {
+        this.options = options;
+        this.fn = function (results, done) {
+            MongoClient.connect('mongodb://'  + this.options.ip + '/' + this.options.dbname, function (err, db) {
+                if (err) {
+                    throw err;
+                }
+                var collection = db.collection(this.options.collection);
+                collection.insert(results, function (err, docs) {
+                    db.close();
+                    done();
+                });
+            });
+        };
+    }
+};
---
+++ tests/index.js
@@ -0,0 +1,10 @@
+(function () {
+    'use strict';
+    var assert = require('assert');
+    var mongo = require("../index.js");
+    describe('test', function () {
+            it('shoud be correct', function () {
+                assert.equal(-1,-1);
+            });
+        });
+}());
