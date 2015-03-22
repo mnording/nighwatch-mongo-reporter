21120538c32b34ec3e7a8b7259e1c4b676ead859
Mnording <mattiasnording@gmail.com>
2015-03-22 20:45:11 +0100

    fixing internal reference

--- index.js
+++ index.js
@@ -3,12 +3,13 @@
 var MongoClient = require('mongodb').MongoClient;
 module.exports = function (options) {
     this.options = options;
+    var self = this;
     this.fn = function (results, done) {
-        MongoClient.connect('mongodb://'  + this.options.ip + '/' + this.options.dbname, function (err, db) {
+        MongoClient.connect('mongodb://'  + self.options.ip + '/' + self.options.dbname, function (err, db) {
             if (err) {
                 throw err;
             }
-            var collection = db.collection(this.options.collection);
+            var collection = db.collection(self.options.collection);
             collection.insert(results, function (err, docs) {
                 db.close();
                 done();
