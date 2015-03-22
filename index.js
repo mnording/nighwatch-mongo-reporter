1c42c75250b3edf8242349646d882cd2ee4d9c00
Mnording <mattiasnording@gmail.com>
2015-03-22 20:26:27 +0100

    finetuning the signature and correcting tests

--- index.js
+++ index.js
@@ -1,20 +1,18 @@
 /*jslint node: true */
 'use strict';
 var MongoClient = require('mongodb').MongoClient;
-module.exports = {
-    MongoReporter : function (options) {
-        this.options = options;
-        this.fn = function (results, done) {
-            MongoClient.connect('mongodb://'  + this.options.ip + '/' + this.options.dbname, function (err, db) {
-                if (err) {
-                    throw err;
-                }
-                var collection = db.collection(this.options.collection);
-                collection.insert(results, function (err, docs) {
-                    db.close();
-                    done();
-                });
+module.exports = function (options) {
+    this.options = options;
+    this.fn = function (results, done) {
+        MongoClient.connect('mongodb://'  + this.options.ip + '/' + this.options.dbname, function (err, db) {
+            if (err) {
+                throw err;
+            }
+            var collection = db.collection(this.options.collection);
+            collection.insert(results, function (err, docs) {
+                db.close();
+                done();
             });
-        };
-    }
+        });
+    };
 };
--- tests/index.js
+++ tests/index.js
@@ -1,10 +1,26 @@
 (function () {
     'use strict';
     var assert = require('assert');
-    var mongo = require("../index.js");
+    var MongoReporter = require("../index.js");
     describe('test', function () {
             it('shoud be correct', function () {
                 assert.equal(-1,-1);
             });
         });
+    describe('Initializer',function() {
+        it('Should not be null after init',function() {
+            var test = new MongoReporter();
+            assert.notEqual(test,undefined);
+        });
+    });
+    describe('Options',function() {
+        it('Should have options',function() { 
+           var test = new MongoReporter({test:"test"});
+                assert.notEqual(test.options.test,undefined);
+           });
+        it('Should have ip',function() { 
+           var test = new MongoReporter({ip:"test"});
+                assert.notEqual(test.options.ip,undefined);
+           });
+    });
 }());
